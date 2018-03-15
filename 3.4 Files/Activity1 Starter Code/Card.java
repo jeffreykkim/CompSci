4a429d0acbcea78d6e7dc5e79d3e58548b56fb2a
Jeffrey Kim <shayminhero@gmail.com>
2018-03-15 12:55:18 -0700

    3.4 Part 1 Done

--- 3.4 Files/Activity1 Starter Code/Card.java
+++ 3.4 Files/Activity1 Starter Code/Card.java
@@ -5,83 +5,92 @@
  */
 public class Card {
 
-	/**
-	 * String value that holds the suit of the card
-	 */
-	private String suit;
+    /**
+     * String value that holds the suit of the card
+     */
+    private String suit;
 
-	/**
-	 * String value that holds the rank of the card
-	 */
-	private String rank;
+    /**
+     * String value that holds the rank of the card
+     */
+    private String rank;
 
-	/**
-	 * int value that holds the point value.
-	 */
-	private int pointValue;
+    /**
+     * int value that holds the point value.
+     */
+    private int pointValue;
+    /**
+     * Creates a new Card instance.
+     *
+     * @param cardRank  a String value
+     *                  containing the rank of the card
+     * @param cardSuit  a String value
+     *                  containing the suit of the card
+     * @param cardPointValue an int value
+     *                  containing the point value of the card
+     */
+    public Card(String cardRank, String cardSuit, int cardPointValue) {
+        rank = cardRank;
+        suit = cardSuit;
+        pointValue = cardPointValue;
+    }
 
+    /**
+     * Accesses this Card's suit.
+     * @return this Card's suit.
+     */
+    public String suit() {
+        return suit;
+    }
 
-   /**
-	 * Creates a new Card instance.
-	 *
-	 * @param cardRank  a String value
-	 *                  containing the rank of the card
-	 * @param cardSuit  a String value
-	 *                  containing the suit of the card
-	 * @param cardPointValue an int value
-	 *                  containing the point value of the card
-	 */
-	public Card(String cardRank, String cardSuit, int cardPointValue) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
+    /**
+     * Accesses this Card's rank.
+     * @return this Card's rank.
+     */
+    public String rank() {
+        return rank;
+    }
 
+    /**
+     * Accesses this Card's point value.
+     * @return this Card's point value.
+     */
+    public int pointValue() {
+        return pointValue;
+    }
 
-	/**
-	 * Accesses this Card's suit.
-	 * @return this Card's suit.
-	 */
-	public String suit() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-   }
+    /** Compare this card with the argument.
+     * @param otherCard the other card to compare to this
+     * @return true if the rank, suit, and point value of this card
+     *              are equal to those of the argument;
+     *         false otherwise.
+     */
+    public boolean matches(Card otherCard) 
+    {
+        if (this.rank() == otherCard.rank() && this.suit() == otherCard.suit() &&
+        this.pointValue() == otherCard.pointValue())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
-	/**
-	 * Accesses this Card's rank.
-	 * @return this Card's rank.
-	 */
-	public String rank() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
-
-   /**
-	 * Accesses this Card's point value.
-	 * @return this Card's point value.
-	 */
-	public int pointValue() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
-
-	/** Compare this card with the argument.
-	 * @param otherCard the other card to compare to this
-	 * @return true if the rank, suit, and point value of this card
-	 *              are equal to those of the argument;
-	 *         false otherwise.
-	 */
-	public boolean matches(Card otherCard) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
-
-	/**
-	 * Converts the rank, suit, and point value into a string in the format
-	 *     "[Rank] of [Suit] (point value = [PointValue])".
-	 * This provides a useful way of printing the contents
-	 * of a Deck in an easily readable format or performing
-	 * other similar functions.
-	 *
-	 * @return a String containing the rank, suit,
-	 *         and point value of the card.
-	 */
-	@Override
-	public String toString() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
+    /**
+     * Converts the rank, suit, and point value into a string in the format
+     *     "[Rank] of [Suit] (point value = [PointValue])".
+     * This provides a useful way of printing the contents
+     * of a Deck in an easily readable format or performing
+     * other similar functions.
+     *
+     * @return a String containing the rank, suit,
+     *         and point value of the card.
+     */
+    @Override
+    public String toString() {
+        return (this.rank() + " of " + this.suit() + "(Point Value = "
+            + this.pointValue() +")");
+    }
 }
